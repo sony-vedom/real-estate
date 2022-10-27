2b05ebb00f5935e687f88e05b317ed98217033b9
sony-vedom <109023595+sony-vedom@users.noreply.github.com>
2022-10-27 17:47:05 +0300

    загрузка

---
+++ js/script.js
@@ -0,0 +1,30 @@
+const burger = document.querySelector('.menu-icon');
+const menu = document.querySelector('.menu');
+const body = document.body;
+
+if (burger && menu) {
+    burger.addEventListener ('click', () => {
+        burger.classList.toggle('_active');
+        menu.classList.toggle('_active');
+        body.classList.toggle('_lock')
+    })
+}
+
+//Filter
+
+const filter = document.querySelector('.filter')
+
+if (filter) {
+    const items = filter.querySelectorAll('.block-filter')
+
+    items.forEach(item => {
+        item.addEventListener('click', event => {
+            item.querySelector(".block-filter__dropdown").classList.toggle('_active');
+            item.querySelector(".block-filter__icon").classList.toggle('_active');
+
+            if (event.target.classList.contains('block-filter__item')) {
+                item.querySelector('.block-filter__value').textContent = event.target.textContent;
+            }
+        })
+    });    
+}
